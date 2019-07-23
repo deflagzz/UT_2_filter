155abc63749805eba92f716e41b7d41b6c2c3435
deflag <1930902187@qq.com>
2019-07-23 16:50:57 +0800

    提交

--- USER/main.c
+++ USER/main.c
@@ -15,24 +15,15 @@ int main(void)
 	//直接写死CAN ID 
 	g_UT_CAN_ID = 17;
 	
-	//mem_init(); 			//初始化内部内存池	
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); 	//设置NVIC中断分组2:2位抢占优先级，2位响应优先级
 	
-	//usart_init(115200);	 	//串口初始化为115200
 	CAN_Mode_Init(CAN_SJW_1tq,CAN_BS2_8tq,CAN_BS1_9tq,4,CAN_Mode_Normal);//CAN初始化环回模式,波特率500Kbps    
 	
 	OLED_Init();
-	//TP_Init();				//触摸屏初始化
 
-	//TFTLCD_Init();			//LCD初始化	
-	//RCC_AHBPeriphClockCmd(RCC_AHBPeriph_CRC,ENABLE);//使能CRC时钟，否则STemWin不能使用 
-	//GUI_Init();
-	//oled_dis();  //oled提示信息
 	
 	TIM3_2PWM_Init(1800-1,1-1);//不分频。PWM频率=38Khz //	IR -- PWM发射初始化  PA7--TIM3--CH2
-	//ir_re_init();//IR接收初始化        PA1--TIM5--CH2	 //c8t6--TIM2CH2
 
-	//	TIM3_3PWM_Init(1800-1,1-1);
 	time2_init(0xFFFF,72-1);//  10us进一次中断
 	extix_init();  //接收--PA1 
 
@@ -43,7 +34,6 @@ int main(void)
 		UT_TX_RX();						//超声波发送接收
 
 		UT_CANSend_u8(g_finale_lenth);	//CAN发送
-		//check_NoSensor();				//OLED显示
 				
 		LED3 = !LED3;
 	}		
